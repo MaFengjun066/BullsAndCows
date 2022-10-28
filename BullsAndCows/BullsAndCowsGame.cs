6c901709761eded63bc827c1c26819c3fea7515d
Fengjun Ma <FMa6@slb.com>
2022-10-28 15:28:23 +0800

    feature: add B' function is to count correct digital but in wrong position

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace BullsAndCows
 {
@@ -16,9 +17,25 @@ namespace BullsAndCows
 
         public string Guess(string guess)
         {
+            int countAllCorrectDigitalIgnorePosition = CountAllCorrectDigitalIgnorePosition(guess);
             int countBulls = CountBulls(guess);
 
-            return $"{countBulls}A0B";
+            return $"{countBulls}A{countAllCorrectDigitalIgnorePosition - countBulls}B";
+        }
+
+        private int CountAllCorrectDigitalIgnorePosition(string guess)
+        {
+            var guessDigits = guess.Split(' ');
+            int countCow = 0;
+            for (var index = 0; index < guessDigits.Length; index++)
+            {
+                if (secret.Contains(guessDigits[index]))
+                {
+                    countCow++;
+                }
+            }
+
+            return countCow;
         }
 
         private int CountBulls(string guess)
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -6,6 +6,17 @@ namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
+        private const string Secret = "1 2 3 4";
+
+        [Fact]
+        public void Should_create_BullsAndCowsGame()
+        {
+            var secretGenerator = new SecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            Assert.NotNull(game);
+            Assert.True(game.CanContinue);
+        }
+
         [Fact]
         public void Should_return_4A0B_when_given_guess_digits_are_same_with_secret()
         {
@@ -19,17 +30,36 @@ namespace BullsAndCowsTest
             Assert.Equal("4A0B", guessResult);
         }
 
-        [Fact]
-        public void Should_return_3A0B_when_given_guess_digits_have_2_correct_digitals_and_2_wrong_digital()
+        [Theory]
+        [InlineData("1 2 3 4", "1 2 5 6")]
+        [InlineData("1 2 3 4", "5 2 3 6")]
+        [InlineData("1 2 3 4", "5 6 3 4")]
+        public void Should_return_2A0B_when_given_guess_digits_have_2_correct_digitals_and_2_wrong_digital(string secret, string guess)
         {
             //given
             var mockSecretGenerator = new Mock<SecretGenerator>();
-            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns("1 2 3 4");
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
             var game = new BullsAndCowsGame(mockSecretGenerator.Object);
             //when
-            var guessResult = game.Guess("1 2 6 5");
+            var guessResult = game.Guess(guess);
             //then
             Assert.Equal("2A0B", guessResult);
+        }
+
+        [Theory]
+        [InlineData("1 2 3 4", "1 5 2 6")]
+        [InlineData("1 2 3 4", "2 5 3 6")]
+        [InlineData("1 2 3 4", "5 3 6 4")]
+        public void Should_return_1A1B_when_given_guess_digits_have_1_correct_digitals_and_1_correct_digital_but_wrong_position(string secret, string guess)
+        {
+            //given
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            //when
+            var guessResult = game.Guess(guess);
+            //then
+            Assert.Equal("1A1B", guessResult);
         }
     }
 }
