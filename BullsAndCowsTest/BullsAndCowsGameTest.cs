4abd5b6b040ba757468cd28d0e5a7afa9095a301
Fengjun Ma <FMa6@slb.com>
2022-10-28 14:44:53 +0800

    features: add A' function to count same digits in same position

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -7,15 +7,6 @@ namespace BullsAndCowsTest
     public class BullsAndCowsGameTest
     {
         [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }
-
-        [Fact]
         public void Should_return_4A0B_when_given_guess_digits_are_same_with_secret()
         {
             //given
@@ -26,6 +17,19 @@ namespace BullsAndCowsTest
             var guessResult = game.Guess("1 2 3 4");
             //then
             Assert.Equal("4A0B", guessResult);
+        }
+
+        [Fact]
+        public void Should_return_3A0B_when_given_guess_digits_have_2_correct_digitals_and_2_wrong_digital()
+        {
+            //given
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            //when
+            var guessResult = game.Guess("1 2 6 5");
+            //then
+            Assert.Equal("2A0B", guessResult);
         }
     }
 }
